a97327a5f9b0377ce553710592c7bb66b92d06ae
Rizwan114 <160789107+Rizwan114@users.noreply.github.com>
2024-02-24 02:30:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var text = "my name is rizwan";
+var Uppercasetext = text.toUpperCase();
+var Lowercasetext = text.toLowerCase();
+console.log(Uppercasetext);
+console.log(Lowercasetext);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let text:string = "my name is rizwan";
+let Uppercasetext:string = text.toUpperCase();
+let Lowercasetext:string = text.toLowerCase();
+
+console.log(Uppercasetext);
+console.log(Lowercasetext);
